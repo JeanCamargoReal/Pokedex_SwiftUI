7a121aba0de8cf65597107e55b9471a28af2788b
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-02-07 17:54:31 -0300

    Criando primeiro card

---
+++ PokedexSwiftUI/PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Jean Camargo on 07/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokedexSwiftUI/PokedexSwiftUI/View/PokemonCell.swift
@@ -0,0 +1,51 @@
+//
+//  PokemonCell.swift
+//  PokedexSwiftUI
+//
+//  Created by Jean Camargo on 07/02/21.
+//
+
+import SwiftUI
+
+struct PokemonCell: View {
+    var body: some View {
+        ZStack {
+            VStack(alignment: .leading) {
+             
+                Text("Bulbasaur")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.top, 8)
+                    .padding(.leading)
+                
+                HStack {
+                    Text("poison")
+                        .font(.subheadline).bold()
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 8)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(Color.white.opacity(0.25))
+                        )
+                        .frame(width: 100, height: 24)
+                    
+                    Image("1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 68, height: 68)
+                        .padding([.bottom, .trailing], 4)
+                }
+            }
+        }
+        .background(Color.green)
+        .cornerRadius(12)
+        .shadow(color: .green, radius: 6, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PokemonCell_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonCell()
+    }
+}
